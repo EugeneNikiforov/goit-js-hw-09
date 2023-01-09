052094724cffcc4ad9317d8e05e79fc11aec2ba2
EugeneNikiforov <115535800+EugeneNikiforov@users.noreply.github.com>
2023-01-09 21:34:56 +0200

    Update 01.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,13 @@
+const startBtn = document.querySelector(`button[data-start]`);
+const stopBtn = document.querySelector(`button[data-stop]`);
+const bodyColor = document.querySelector(`body`);
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
 
+stopBtn.setAttribute(`disabled`, true);
+startBtn.addEventListener(`click`, changeBodyColor);
+
+function changeBodyColor() {
+    stopBtn.removeAttribute(`disabled`);
+}
