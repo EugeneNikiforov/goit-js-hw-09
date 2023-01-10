f5fce6c38c5132df2f17ead4e37f0a89ca99540b
EugeneNikiforov <115535800+EugeneNikiforov@users.noreply.github.com>
2023-01-10 19:58:32 +0200

    Summary update

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,57 @@
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+// one by one
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const dataDays = document.querySelector(`span[data-days]`);
+const dataHours = document.querySelector(`span[data-hours]`);
+const dataMinutes = document.querySelector(`span[data-minutes]`);
+const dataSeconds = document.querySelector(`span[data-seconds]`);
+const input = document.querySelector(`input#datetime-picker`);
+const startBtn = document.querySelector(`button[data-start]`);
 
 
+startBtn.setAttribute(`disabled`, true);
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+      console.log(selectedDates[0]);
+      if (selectedDates[0] < new Date()) {
+          return Notify.failure("Please choose a date in the future");
+      };
+      startBtn.removeAttribute(`disabled`);
+      startBtn.addEventListener(`click`, (() => {
+          const timer = selectedDates[0].getTime();
+          const timerStart = setInterval(() => {
+              const deltaTime = timer - new Date();
+              const time = convertMs(deltaTime);
+              if (timer - new Date() > 0) {
+                  updateTime(time);
+                  startBtn.setAttribute(`disabled`, true);
+                  input.setAttribute(`disabled`, true);
+              } else {
+                  clearInterval(timerStart);
+                  return Notify.success('Done  (O_O)');
+              }
+          }, 1000)
+      }))
+  },
+};
+flatpickr(`input`, options);
 
+function updateTime({ days, hours, minutes, seconds }) {
+    dataDays.textContent = `${days}`;
+    dataHours.textContent = `${hours}`;
+    dataMinutes.textContent = `${minutes}`;
+    dataSeconds.textContent = `${seconds}`;
+};
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, `0`);
+};
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -9,13 +60,19 @@ function convertMs(ms) {
   const day = hour * 24;
 
   // Remaining days
-  const days = Math.floor(ms / day);
+  const days = addLeadingZero(Math.floor(ms / day));
   // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
   // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
 };
+// const fieldEl = document.querySelectorAll('.field')
+// refs.fieldEl.forEach(el => {
+//   el.style.display = 'flex';
+//   el.style.flexDirection = 'column';
+//   el.style.justifyContent = 'center';
+// });
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay })
+      } else {
+        reject({ position, delay })
+      }
+    }, delay)
+  });
+};
+
+const form = document.querySelector(`.form`);
+
+form.addEventListener(`submit`, submitForm);
+let delay = 0;
+let position = 0;
+function submitForm(e) {
+  e.preventDefault();
+  const formItem = e.currentTarget.elements;
+  const firstDelay = Number(formItem.delay.value);
+  const step = Number(formItem.step.value);
+  const amount = Number(formItem.amount.value);
+  makePromises(firstDelay, step, amount);
+};
+function makePromises(firstDelay, step, amount) {
+  for (let i = 1; i <= amount; i += 1) {
+    if (i === 1) {
+      delay = firstDelay;
+    } else {
+      delay += step;
+    };
+    position = i;
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`)
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`)
+      });
+  };
+};
+
